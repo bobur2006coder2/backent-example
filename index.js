d5fead86d313886d331a61ef749923a9dd35fec5
bobur2006coder <boburdilmurodov702@gmail.com>
2023-06-23 20:31:05 +0500

    gfgh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonSever = require("json-server");
 const server = jsonSever.create();
 const router = jsonSever.router("db.json");
 const middlewares = jsonSever.defaults();
-const port = process.env.PORT || 2010;
+const port = process.env.PORT || 3050;
 server.use(middlewares);
 server.use(router);
 server.listen(port);
